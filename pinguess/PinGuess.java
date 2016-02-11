73c006be09cf5d0fefc683b02ad22c9e2e4abc00
matthewbbrumley <matt@syntropy.io>
2016-02-11 16:06:05 -0500

    First Commit

---
+++ pinguess/PinGuess.java
@@ -0,0 +1,17 @@
+public class PinGuess {
+    
+    //Variable Declaration
+    
+    //Method Declaration
+    //main is the function that runs first and starts the lifecycle
+    public static void main(String[] args) {
+        
+        String pinNumber = "5412";
+        
+        /*
+         Hint: Rewrite the leftPin Function from the js method into here
+         
+         */
+    }
+    
+}
